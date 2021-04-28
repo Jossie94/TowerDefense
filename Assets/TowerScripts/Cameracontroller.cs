263d6b7de4f09fb858152990bbd9327757e22215
Jossie94 <josefine.sodergard@hotmail.com>
2021-04-28 14:18:28 +0200

    started shop

--- Assets/TowerScripts/Cameracontroller.cs
+++ Assets/TowerScripts/Cameracontroller.cs
@@ -7,6 +7,9 @@ public class Cameracontroller : MonoBehaviour
     private bool doMovement = true;
     public float panSpeed = 30f;
     public float panBorderThickness = 10f;
+    public float scrollSpeed = 5f;
+    public float minY = 10f;
+    public float maxY = 80f;
 
     // Update is called once per frame
     void Update()
@@ -41,6 +44,11 @@ public class Cameracontroller : MonoBehaviour
         float scroll = Input.GetAxis("Mouse ScrollWheel");
 
         Vector3 pos = transform.position;
+
+        pos.y -= scroll * 1000* scrollSpeed * Time.deltaTime;
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+
+        transform.position = pos;
 
     }
 }
---
+++ Assets/TowerScripts/Shop.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    public void PurchaseStandardTurret()
+    {
+        Debug.Log("Standard Turret Purchased");
+    }
+
+    public void PurchaseAnotherTurret()
+    {
+        Debug.Log("Another Turret Purchased");
+    }
+}
+
